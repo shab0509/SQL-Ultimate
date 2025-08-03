5e77ac28caf3df1f804d1b5729e222bca49c01bb
Shabi Victor <38999931+shab0509@users.noreply.github.com>
2025-08-04 03:36:24 +0530

    Release : Updated Content

--- scripts/03_Data_Manipulation_DML.sql
+++ scripts/03_Data_Manipulation_DML.sql
@@ -1,7 +1,7 @@
 /* ==============================================================================
    SQL Data Manipulation Language (DML)
 -------------------------------------------------------------------------------
-   This guide covers the essential DML commands used for inserting, updating, 
+   This gucustomeride covers the essential DML commands used for inserting, updating, 
    and deleting data in database tables.
 
    Table of Contents:
@@ -16,23 +16,23 @@
 =============================================================================== */
 /* #1 Method: Manual INSERT using VALUES */
 -- Insert new records into the customers table
-INSERT INTO customers (id, first_name, country, score)
+INSERT INTO customers (customerid, firstname, country, score)
 VALUES 
     (6, 'Anna', 'USA', NULL),
     (7, 'Sam', NULL, 100)
 
 -- Incorrect column order 
-INSERT INTO customers (id, first_name, country, score)
+INSERT INTO customers (customerid, firstname, country, score)
 VALUES 
     (8, 'Max', 'USA', NULL)
     
 -- Incorrect data type in values
-INSERT INTO customers (id, first_name, country, score)
+INSERT INTO customers (customerid, firstname, country, score)
 VALUES 
 	('Max', 9, 'Max', NULL)
 
 -- Insert a new record with full column values
-INSERT INTO customers (id, first_name, country, score)
+INSERT INTO customers (customerid, firstname, country, score)
 VALUES (8, 'Max', 'USA', 368)
 
 -- Insert a new record without specifying column names (not recommended)
@@ -40,17 +40,17 @@ INSERT INTO customers
 VALUES 
     (9, 'Andreas', 'Germany', NULL)
     
--- Insert a record with only id and first_name (other columns will be NULL or default values)
-INSERT INTO customers (id, first_name)
+-- Insert a record with only customerid and firstname (other columns will be NULL or default values)
+INSERT INTO customers (customerid, firstname)
 VALUES 
     (10, 'Sahra')
 
 /* #2 Method: INSERT DATA USING SELECT - Moving Data From One Table to Another */
 -- Copy data from the 'customers' table into 'persons'
-INSERT INTO persons (id, person_name, birth_date, phone)
+INSERT INTO persons (customerid, person_name, birth_date, phone)
 SELECT
-    id,
-    first_name,
+    customerid,
+    firstname,
     NULL,
     'Unknown'
 FROM customers
@@ -59,16 +59,16 @@ FROM customers
    UPDATE
 =============================================================================== */
 
--- Change the score of customer with ID 6 to 0
+-- Change the score of customer with customerid 6 to 0
 UPDATE customers
 SET score = 0
-WHERE id = 6
+WHERE customerid = 6
 
--- Change the score of customer with ID 10 to 0 and update the country to 'UK'
+-- Change the score of customer with customerid 10 to 0 and update the country to 'UK'
 UPDATE customers
 SET score = 0,
     country = 'UK'
-WHERE id = 10
+WHERE customerid = 10
 
 -- Update all customers with a NULL score by setting their score to 0
 UPDATE customers
@@ -84,17 +84,22 @@ WHERE score IS NULL
    DELETE
 =============================================================================== */
 
--- Select customers with an ID greater than 5 before deleting
+-- Select customers with an customerid greater than 5 before deleting
 SELECT *
 FROM customers
-WHERE id > 5
+WHERE customerid > 5
 
--- Delete all customers with an ID greater than 5
+-- Delete all customers with an customerid greater than 5
 DELETE FROM customers
-WHERE id > 5
+WHERE customerid > 5
 
 -- Delete all data from the persons table
 DELETE FROM persons
+
+
+/* ============================================================================== 
+   TRUNCATE
+=============================================================================== */
 
 -- Faster method to delete all rows, especially useful for large tables
 TRUNCATE TABLE persons
